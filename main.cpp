1047e8d9ca974f934afb75b6265677e2f98b54ad
SLokyan <lokyan2018@mail.ru>
2023-12-28 07:01:36 +0300

    Добавлен ввод двух целых чисел

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "sdt.h"
 
 int main(){
-
+    int inp1, inp2;
+    printf("¬ведите первое число: \n");
+    scanf("%d", &inp1);
+    printf("¬ведите второе число: \n");
+    scanf("%d", &inp2);
+    return 1;
 }
